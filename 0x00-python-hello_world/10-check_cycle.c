ee2cdb2f0bbba251fae57eaeb94c8f7b1abecd70
thaikum <fredthaiku@gmail.com>
2023-06-05 14:37:46 -0700

    adding cyclic

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,64 +1,4 @@
 #include "lists.h"
-
-/**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
-
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
-
-	return (n);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-	listint_t *current;
-
-	while (head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
-}
 /**
  * check_exists - checks if a node exists in a linked list
  * @list: the list head
@@ -85,7 +25,6 @@ int check_exists(listint_t *list, listint_t *test, listint_t *terminal)
  */
 int check_cycle(listint_t *list)
 {
-	int x;
 	listint_t *head = list;
 
 	if (!head)
@@ -107,4 +46,5 @@ int check_cycle(listint_t *list)
 				return (0);
 		}
 	}
+	return (0);
 }
