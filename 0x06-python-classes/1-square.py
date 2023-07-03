edcd12fae14dc61ba4fd470db7daaeed2899850a
thaikum <fredthaiku@gmail.com>
2023-07-03 14:12:40 -0700

    documenting

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
- This module creates a square and initializes it with 
+ This module creates a square and initializes it with
  an integer value
 """
+
+
 class Square:
     """
     the constructor initializes size
     as an instance variable
     """
-    
+
     def __init__(self, size: int):
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Squeare class
+"""
+
+
 class Square:
+    """
+    initialization and type checking
+    """
+    
     def __init__(self, size=0):
         if (type(size) != int):
             raise TypeError("size must be an integer")
