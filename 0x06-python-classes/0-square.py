afe6e5bc7cbde86388cdf159765482ef9cc559c4
thaikum <fredthaiku@gmail.com>
2023-07-03 14:05:45 -0700

    documenting class 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-This module declares an emty class square
-
-Author: Fredrick Thaiku
-Version: 1.0
-"""
 class Square:
+    """
+    This module declares an emty class square
+
+    Author: Fredrick Thaiku
+    Version: 1.0
+    """
     pass
