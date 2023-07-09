20faeb2419be78c594cfdb1400695284a3648a1f
thaikum <fredthaiku@gmail.com>
2023-07-09 13:18:33 -0700

    29 fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-def magic_string(n=1):
-    return ", ".join(["Best School" for x in range(n)])
+def magic_string(n=0):
+    return ", ".join(["Best School" for x in range(n + 1)])
