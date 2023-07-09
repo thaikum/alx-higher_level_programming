8b3713ff3e9d152949b3d8acb6d6ed3bc01cfb18
thaikum <fredthaiku@gmail.com>
2023-07-09 13:39:15 -0700

    fix static

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string(n=0):
-    return ", ".join(["BestSchool" for _ in range(n + 1)])
+def magic_string():
+    magic_string.n = magic_string.n + 1 if hasattr(magic_string, "n") else 0
+    return ", ".join(["BestSchool" for _ in range(magic_string.n + 1)])
