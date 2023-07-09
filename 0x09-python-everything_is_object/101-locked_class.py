7c4cf4fc24209ca7dea09f4e7ca1584afcf944d2
thaikum <fredthaiku@gmail.com>
2023-07-09 13:47:46 -0700

    add documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+jasdkfjalj
+"""
 class LockedClass:
+    """
+    askldfjla
+    """
     def __setattr__(self, name, value):
         if hasattr(self, name) or name == "first_name":
             super().__setattr__(name, value)
