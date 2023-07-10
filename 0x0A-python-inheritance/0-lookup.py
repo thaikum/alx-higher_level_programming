2049471f9f3a54b679bd8167c48155932f6ea280
thaikum <fredthaiku@gmail.com>
2023-07-10 13:24:30 -0700

    lsjafl

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+this module contains a lookip method to return
+all methods and attributes in a class
+"""
+
+
+def lookup(obj):
+    """
+    dir
+    """
+    return dir(obj)
