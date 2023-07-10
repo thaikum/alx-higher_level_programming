ae22fae2bca41b1a5f9be17a59461f9741bf40b3
thaikum <fredthaiku@gmail.com>
2023-07-10 13:51:20 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class my_list
+"""
+
+
+class MyList(list):
+    """
+    inheriting list
+    """
+
+    def print_sorted(self):
+        print(sorted(self, reverse=True))
