1efbe757d6e9e4251bed0e8e8f94a803e7e24e57
thaikum <fredthaiku@gmail.com>
2023-07-12 07:44:11 -0700

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    square
+    """
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
