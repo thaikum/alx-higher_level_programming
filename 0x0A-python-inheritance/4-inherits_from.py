20a9fcb29db5ff49848605e3c2189ccfce268cda
thaikum <fredthaiku@gmail.com>
2023-07-12 05:47:00 -0700

    fix same class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """
     if it inherits return true
     """
-    return issubclass(obj.__class__, a_class)
+    return issubclass(obj.__class__, a_class) and type(obj) != a_class
