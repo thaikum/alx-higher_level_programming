2d953b5a475e1938ad8bcca2b85c2137d3b95662
thaikum <fredthaiku@gmail.com>
2023-07-12 05:36:54 -0700

    3rd task

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,4 +9,4 @@ def is_same_class(obj, a_class):
     """
     is instance
     """
-    return isinstance(obj, a_class)
+    return type(obj) == a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Is parent or child
+"""
+def is_kind_of_class(obj, a_class):
+    """
+    is parent or child
+    """
+    return isinstance(obj, a_class)
