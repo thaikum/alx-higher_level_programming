f6cb9d0bca40eaa786a8c6760344d34ecf1c54a2
thaikum <fredthaiku@gmail.com>
2023-07-12 05:40:41 -0700

    task 4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,8 @@
 """
 Is parent or child
 """
+
+
 def is_kind_of_class(obj, a_class):
     """
     is parent or child
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+issubclass module
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    if it inherits return true
+    """
+    return issubclass(obj, a_class)
