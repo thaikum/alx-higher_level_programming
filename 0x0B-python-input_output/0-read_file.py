779d01467f544372f3a8be0214352130f90cb453
thaikum <fredthaiku@gmail.com>
2023-07-13 11:29:46 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+reading files
+"""
+
+
+def read_file(filename=""):
+    """
+    file reader
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
