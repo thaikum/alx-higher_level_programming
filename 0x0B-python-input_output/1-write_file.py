ada3d62df636b25aca70dfb0e6b8a49fbb23aa84
thaikum <fredthaiku@gmail.com>
2023-07-13 11:39:08 -0700

    writer

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,5 +10,4 @@ def write_file(filename="", text=""):
     writer function
     """
     with open(filename, "w", encoding="utf-8") as file:
-        file.write(text)
-        return len(text)
+        return (file.write(text))
