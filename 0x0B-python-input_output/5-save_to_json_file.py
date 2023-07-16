0bc91be92086b55b69fed55226d74e89e64c9453
thaikum <fredthaiku@gmail.com>
2023-07-16 04:02:15 -0700

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,7 +10,7 @@ def save_to_json_file(my_obj, filename):
     """
     json writer
     """
-    
+
     try:
         with open(filename, "w", encoding="utf-8") as file:
             json.dump(my_obj, file, ensure_ascii=False)
