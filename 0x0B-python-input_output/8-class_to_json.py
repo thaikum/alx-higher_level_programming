e50cf86ef5d8732662b38a346b38ccdaa3a47132
thaikum <fredthaiku@gmail.com>
2023-07-16 04:29:34 -0700

    clas to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class to json
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    class to json
+    """
+    attributes = {}
+    for attr in obj.__dict__:
+        value = getattr(obj, attr)
+        if isinstance(value, (list, dict, str, int, bool)):
+            attributes[attr] = value
+    return str(attributes)
